59f2b0d3812785ef6ec3a25d887235b01d763baa
Finn Vilsbæk <finnvs@users.noreply.github.com>
2018-09-07 10:59:50 +0200

    Add files via upload

---
+++ Scripts/Avatar/avatar.js
@@ -0,0 +1,399 @@
+﻿// ================================================================
+//  Description: Avatar Upload supporting script
+//  License:     MIT - check License.txt file for details
+//  Author:      Codative Corp. (http://www.codative.com/) 
+//  with some site specific changes by Panmedia (www.panmedia.dk)
+// ================================================================
+var jcrop_api,
+    boundx,
+    boundy,
+    xsize,
+    ysize;
+
+// ToDo - change the size limit of the file. You may need to change web.config if larger files are necessary.
+var maxSizeAllowed = 5;     // Upload limit in MB
+var maxSizeInBytes = maxSizeAllowed * 1024 * 1024;
+var keepUploadBox = false;  // ToDo - Remove if you want to keep the upload box
+var keepCropBox = false;    // ToDo - Remove if you want to keep the crop box
+var rotateValue = 0;
+// fix for paths where an absolute url is needed
+var environ = window.location.host;
+if (environ === "localhost:30321") {
+    var baseurl = window.location.protocol + "//" + window.location.host + "/" + "OrchardLocal/";
+} else {
+    var baseurl = window.location.protocol + "//" + window.location.host + "/";
+}
+// alert("BaseURL: " + baseurl);
+
+var progressCustom = new LoadingOverlayProgress({
+    bar: {
+        "background": 'red',
+        "top": "50px",
+        "height": "30px",
+        "border-radius": "15px"
+    },
+    text: {
+        "color": 'red',
+        "font-family": "monospace",
+        "top": "25px"
+    }
+});
+
+// example - call specific function only when window has loaded
+// window.addEventListener('load', pageInit);
+
+// $(function () {
+function pageInit() {
+    if (typeof $('#avatar-upload-form') !== undefined) {
+        initAvatarUpload();
+        $('#avatar-max-size').html(maxSizeAllowed);
+        $('#avatar-upload-form button[id=cancel_btn]').click(function () {
+            cancel_btn_handler();
+        });
+        $('#avatar-upload-form input:file').on("change", function (e) {
+            var files = e.currentTarget.files;
+            for (var x in files) {
+                if (files[x].name !== "item" && typeof files[x].name !== "undefined") {
+                    if (files[x].size <= maxSizeInBytes) {
+                        // Submit the selected file
+                        $('#avatar-upload-form .upload-file-notice').removeClass('bg-danger');
+                        // alert("Submitting file");
+                        $('#avatar-upload-form').submit();
+                    } else {
+                        // File too large
+                        $('#avatar-upload-form .upload-file-notice').addClass('bg-danger');
+                    }
+                }
+            }           
+        });
+    }
+}
+
+
+function initAvatarUpload() {
+    // alert("initAvatarUpload was called.")
+    $('#avatar-upload-form').ajaxForm({
+
+        // standard progress bar
+        //beforeSend: function () {
+        //    updateProgress(0);
+        //    $('#avatar-upload-form').addClass('hidden');
+        //},
+        //uploadProgress: function (event, position, total, percentComplete) {
+        //    updateProgress(percentComplete);
+        //},
+
+        // overlay page while uploading
+        beforeSend: function () {
+            $.LoadingOverlay("show", {
+                custom: progressCustom.Init()
+            });
+        },        
+        success: function (data) {
+            updateProgress(100);
+            if (data.success === false) {
+                $('#status').html(data.errorMessage);
+            } else {
+                // alert("Success handler initiated");
+                $('#preview-pane .preview-container img').attr('src', data.fileName);
+                var img = $('#crop-avatar-target');
+                img.attr('src', data.fileName);
+
+                if (!keepUploadBox) {
+                    $('#avatar-upload-box').addClass('hidden');
+                }
+                $('#avatar-crop-box').removeClass('hidden');
+                initAvatarCrop(img);
+            }
+        },
+
+        error: function (data) {
+            alert("Error handler executing:" + data.responsetext);
+        },
+
+        complete: function (xhr) {
+            $.LoadingOverlay("hide");
+        }
+    });
+}
+
+function updateProgress(percentComplete) {
+    $('.upload-percent-bar').width(percentComplete + '%');
+    $('.upload-percent-value').html(percentComplete + '%');
+    if (percentComplete === 0) {
+        $('#upload-status').empty();
+        $('.upload-progress').removeClass('hidden');
+    }
+}
+
+function initAvatarCrop(img) {
+    img.Jcrop({
+        onChange: updatePreviewPane,
+        onSelect: updatePreviewPane,
+        aspectRatio: xsize / ysize
+    }, function () {
+        var bounds = this.getBounds();
+        boundx = bounds[0];
+        boundy = bounds[1];
+
+        jcrop_api = this;
+        jcrop_api.setOptions({ allowSelect: true });
+        jcrop_api.setOptions({ allowMove: true });
+        jcrop_api.setOptions({ allowResize: true });
+        jcrop_api.setOptions({ aspectRatio: 1 });
+
+        // Maximise initial selection around the centre of the image,
+        // but leave enough space so that the boundaries are easily identified.
+        var padding = 10;
+        var shortEdge = (boundx < boundy ? boundx : boundy) - padding;
+        var longEdge = boundx < boundy ? boundy : boundx;
+        var xCoord = longEdge / 2 - shortEdge / 2;
+        jcrop_api.animateTo([xCoord, padding, shortEdge, shortEdge]);
+
+        var pcnt = $('#preview-pane .preview-container');
+        xsize = pcnt.width();
+        ysize = pcnt.height();
+        $('#preview-pane').appendTo(jcrop_api.ui.holder);
+        jcrop_api.focus();
+    });
+    // call the ghostbusters, show rotate buttons            
+    initRotateAvatar();
+    $('#rotate-apply').removeClass('hidden');
+    $('#rotate-minus90').removeClass('hidden');
+    $('#rotate-180').removeClass('hidden');
+    $('#rotate-cancel').removeClass('hidden');
+}
+
+function updatePreviewPane(c) {
+    if (parseInt(c.w) > 0) {
+        var rx = xsize / c.w;
+        var ry = ysize / c.h;
+
+        $('#preview-pane .preview-container img').css({
+            width: Math.round(rx * boundx) + 'px',
+            height: Math.round(ry * boundy) + 'px',
+            marginLeft: '-' + Math.round(rx * c.x) + 'px',
+            marginTop: '-' + Math.round(ry * c.y) + 'px'
+        });
+    }
+}
+
+function saveAvatar() {
+    var img = $('#preview-pane .preview-container img');
+    // add AntiforgeryToken
+    var form = $('#avatar-upload-form')[0];
+    var token = $('input[name="__RequestVerificationToken"]', form).val();
+    // alert("Value of AF token: " + token); // works as advertised! 
+    $('#avatar-crop-box button').addClass('disabled');
+
+    $.ajax({
+        type: "POST",        
+        // url: "Avatar/Save",  // dette skal være en absolut url, ellers kommer Orchards routing ikke i spil
+        // og requestet vil gå til /OrchardLocal/Panmedia.Artist/Artist/Avatar/Save, som ikke findes        
+        // url: 'http://localhost:30321/OrchardLocal/Avatar/Save',  // Controller Avatar, method Save - url håndteres af Orchard routing
+        url: baseurl + 'Avatar/Save',
+        traditional: true,
+
+        beforeSend: function () {
+            $.LoadingOverlay("show", {
+                custom: progressCustom.Init()
+            });
+        },
+        complete: function () {
+            $.LoadingOverlay("hide");
+        },
+
+        data: {
+            w: img.css('width'),
+            h: img.css('height'),
+            l: img.css('marginLeft'),
+            t: img.css('marginTop'),
+            fileName: img.attr('src'),
+            rotateVal: rotateValue,
+            __RequestVerificationToken: token
+        }
+    }).done(function (data) {
+        if (data.success === true) {
+            $('#avatar-result img').attr('src', data.avatarFileLocation);
+            $('#avatar-result').removeClass('hidden');
+            if (!keepCropBox) {
+                $('#avatar-crop-box').addClass('hidden');
+            }
+            if (data.redirecturl) {
+                // alert("Redirect string: " + data.redirecturl);
+                window.location.href = baseurl + data.redirecturl;
+            }
+        } else {
+            alert(data.errorMessage);
+        }
+    }).fail(function (e) {
+        alert('Cannot upload avatar at this time - current baseurl is : ' + baseurl);
+    });
+}
+
+function initRotateAvatar() {
+    var img = $('#preview-pane .preview-container img');    
+    $('#rotate-apply').on('click', function () {
+        // simplest possible call to jQueryRotate library - and it works!        
+        rotateValue = 90;
+        img.rotate(90);        
+    });
+
+    $('#rotate-minus90').on('click', function () {               
+        rotateValue = -90;
+        img.rotate(-90);
+    });
+
+    $('#rotate-180').on('click', function () {
+        rotateValue = 180;
+        img.rotate(180);
+    });
+
+    $('#rotate-cancel').on('click', function () {                
+        rotateValue = 0;
+        img.rotate(0);
+    });
+
+    // Canvas implementation as used in Orchard.ImageEditor - not fully functional atm
+
+    //var pcnt = $('#preview-pane .preview-container');
+    //xsize = pcnt.width();
+    //ysize = pcnt.height();
+
+    // var img = $('#preview-pane .preview-container img');
+    //alert("img src attr: " + img.attr("src"));
+    //alert("pcnt height: " + ysize);
+    //alert("img css height: " + img.css("height"));
+
+    //$('#rotate-apply').on('click', function () {
+    //    var image = new Image();
+    //    image.onload = function () {
+    //        var canvas = document.createElement('canvas');
+    //        var degrees = "90"; // $("#rotate-angle").val();
+
+    //        if (degrees != "180") {
+    //            //canvas.width = img.css("height");
+    //            //canvas.height = img.css("width");
+    //            canvas.width = ysize; // originally, w and h are interchanged to h and w
+    //            canvas.height = xsize;
+    //            //canvas.width = xsize; // experiment to see what will happen keeping w and h - image disappears in preview :-)
+    //            //canvas.height = ysize;
+    //        } else {
+    //            canvas.width = img.css("width");
+    //            canvas.height = img.css("height");
+    //        }
+
+    //        $(img).css({
+    //            width: canvas.width,
+    //            height: canvas.height
+    //        });
+
+    //        var context = canvas.getContext('2d');
+
+    //        context.translate(canvas.width / 2, canvas.height / 2);
+    //        context.rotate(degrees * Math.PI / 180);
+    //        context.drawImage(image, -image.width / 2, -image.height / 2, image.width, image.height);
+
+    //        // img src repræsenterer nu det roterede billede. Det ændres, men drejer bare ikke..
+    //        img.src = canvas.toDataURL("image/png");           
+            
+    //    };
+    //    image.src = img.attr("src"); // img.src; er undefined så vi bruger url istedet
+    //});
+}
+
+function uploadAvatar() {    
+    var form = $('#avatar-upload-form')[0];
+    // var selectedFile = ($("#UploadedFile"))[0].files[0];
+    var img = $('#preview-pane .preview-container img');
+    var token = $('input[name="__RequestVerificationToken"]', form).val();
+    // create a canvas, fill it with the cropped / rotated 'img' 
+    // and utilize canvas.toDataUrl function in order to send a blob-as-file    
+    var canvas = document.createElement('canvas');    
+    var context = canvas.getContext('2d');
+    context.drawImage(img[0], 0, 0);
+    var dataUrl = canvas.toDataURL("image/png");
+    var dataString = new FormData();
+    var blob = dataURItoBlob(dataUrl);
+    // hmm.this will send a file with filename 'blob'.. and not tilted, even if rotated?   
+    dataString.append("file", blob);
+    dataString.append("filename", img[0].fileName);
+    // send token-dude as wella
+    dataString.append("__RequestVerificationToken", token);
+
+    var progressCustom = new LoadingOverlayProgress({
+        bar: {
+            "background": '#000', // red,
+            "top": "50px",
+            "height": "30px",
+            "border-radius": "15px"
+        },
+        text: {
+            "color": '#000', // red,
+            "font-family": "monospace",
+            "top": "25px"
+        }
+    });
+
+    $.ajax({
+        // url: 'http://localhost:30321/OrchardLocal/Profile/ImageEdit',  //Server script to process data
+        url: baseurl + 'Profile/ImageEdit',
+        type: 'POST',
+        beforeSend: function () {
+            $.LoadingOverlay("show", {
+                custom: progressCustom.Init()
+            });
+        },
+        complete: function () {
+            $.LoadingOverlay("hide");
+        },
+        xhr: function () { 
+            var myXhr = $.ajaxSettings.xhr();           
+            return myXhr;
+        },
+        //Ajax events
+        success: successHandler,
+        error: errorHandler,        
+        data: dataString,
+        //Options to tell jQuery not to process data or worry about content-type.
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+}
+
+function successHandler(response) {
+   
+    if (response.redirect) {
+        window.location.href = response.redirect;
+    }
+    else {
+        // Huu - ballade. Redirect..
+        window.location = baseurl;  //'http://localhost:30321/OrchardLocal/ProfileRedirect';
+    }
+}
+
+function errorHandler(xhr, ajaxOptions, thrownError) {
+    alert("There was an error attempting to upload the file. (" + thrownError + ")");
+}
+
+// support sending a cropped and rotated image as a blob -
+// ref http://stackoverflow.com/questions/18253378/javascript-blob-upload-with-formdata
+
+function dataURItoBlob(dataURI) {
+    var byteString = atob(dataURI.split(',')[1]);
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+    var bb = new Blob([ab], { "type": mimeString });
+    return bb;
+}
+
+// go to site root on cancel image upload
+function cancelImageUpload() {    
+    window.location = baseurl;
+}
+
---
+++ Scripts/imageupload.js
@@ -0,0 +1,289 @@
+﻿function Init_Upload() {
+    // alert("Init Upload called");
+    $('#FormUpload input[name=UploadedFile]').change(function (evt) { singleFileSelected(evt); });
+    $("#FormUpload button[id=Cancel_btn]").click(function () {
+        Cancel_btn_handler();
+    });
+    $('#FormUpload button[id=Submit_btn]').click(function () {
+        UploadFile();
+    });
+
+
+    //$.blockUI.defaults.overlayCSS = {
+    //    backgroundColor: '#000',
+    //    opacity: 0.6
+    //};
+    //$.blockUI.defaults.css = {
+    //    padding: 0,
+    //    margin: 5,
+    //    width: '50%',
+    //    top: '30%',
+    //    left: '25%',
+    //    color: '#000',
+    //    border: '3px solid #aaa',
+    //    backgroundColor: '#fff'
+    //};
+    //$.blockUI({ message: $('#createView') });
+}
+
+function singleFileSelected(evt) {
+    //var selectedFile = evt.target.files can use this or select input file element and access it's files object
+    var selectedFile = ($("#UploadedFile"))[0].files[0]; //FileControl.files[0];
+    if (selectedFile) {
+        var FileSize = 0;
+        var imageType = /image.*/;
+        if (selectedFile.size > 1048576) {
+            FileSize = Math.round(selectedFile.size * 100 / 1048576) / 100 + " MB";
+        }
+        else if (selectedFile.size > 1024) {
+            FileSize = Math.round(selectedFile.size * 100 / 1024) / 100 + " KB";
+        }
+        else {
+            FileSize = selectedFile.size + " Bytes";
+        }
+
+        if (selectedFile.type.match(imageType)) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+
+                $("#Imagecontainer").empty();
+                var dataURL = reader.result;
+                var img = new Image();
+                img.src = dataURL;
+                img.className = "thumb";
+                $("#Imagecontainer").append(img);
+
+                // add cropping ability to Image container and fade in cropimage area / btn
+                initCrop();
+                // alert("Init Crop called");
+                $("#crop-image-area").fadeIn("slow");
+                alert("Image File Path:" + selectedFile);
+                $("#hf-uploaded-image-path").val(selectedFile); // needed ref for AJAX post to controller
+
+            };
+            reader.readAsDataURL(selectedFile);
+        }
+        $("#FileName").text("Name : " + selectedFile.name);
+        $("#FileType").text("type : " + selectedFile.type);
+        $("#FileSize").text("Size : " + FileSize);
+    }
+}
+
+function UploadFile() {
+    //we can create form by passing the form to Constructor of formData object
+    //or creating it manually using append function 
+    //but please note file name should be same like the action Parameter
+    //var dataString = new FormData();
+    //dataString.append("UploadedFile", selectedFile);
+
+    var form = $('#FormUpload')[0];
+    var selectedFile = ($("#UploadedFile"))[0].files[0];
+    var token = $('input[name="__RequestVerificationToken"]', form).val();
+    // var dataString = new FormData(form);
+    var dataString = new FormData();
+    dataString.append("UploadedFile", selectedFile);
+    // send token-dude as wella
+    dataString.append("__RequestVerificationToken", token);
+
+    var progressCustom = new LoadingOverlayProgress({
+        bar: {
+            "background": '#000', // red,
+            "top": "50px",
+            "height": "30px",
+            "border-radius": "15px"
+        },
+        text: {
+            "color": '#000', // red,
+            "font-family": "monospace",
+            "top": "25px"
+        }
+    });
+
+    $.ajax({
+        url: 'http://localhost:30321/OrchardLocal/Profile/ImageEdit',  //Server script to process data
+        type: 'POST',
+        beforeSend: function () {
+            $.LoadingOverlay("show", {
+                custom: progressCustom.Init()
+            });
+        },
+        complete: function () {
+            $.LoadingOverlay("hide");
+        },
+        xhr: function () {  // Custom XMLHttpRequest
+            var myXhr = $.ajaxSettings.xhr();
+            // 01.03.2017 test taget ud for at undgå standard prog bar ved upload til browser af thumb
+            //if (myXhr.upload) { // Check if upload property exists                
+            //    myXhr.upload.addEventListener('progress', progressHandlingFunction, false); // For handling the progress of the upload
+            //}
+            return myXhr;
+        },
+        //Ajax events
+        success: successHandler,
+        error: errorHandler,
+        // complete: completeHandler, // would double up re loader
+        // Form data
+        data: dataString,
+        //Options to tell jQuery not to process data or worry about content-type.
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+}
+
+
+// 01.03.2017 test taget ud for at undgå standard prog bar ved upload til browser af thumb
+//function progressHandlingFunction(e) {
+//    if (e.lengthComputable) {
+//        var percentComplete = Math.round(e.loaded * 100 / e.total);
+//        $("#FileProgress").css("width", percentComplete + '%').attr('aria-valuenow', percentComplete);
+//        $('#FileProgress span').text(percentComplete + "%");
+//    }
+//    else {
+//        $('#FileProgress span').text('unable to compute');
+//    }
+//}
+
+
+function completeHandler() {
+    $('#createView').empty();
+    $('.CreateLink').show();
+    // $.unblockUI();
+}
+
+
+//function successHandler(data) {
+function successHandler(response) {
+    // data statuscode er undefined ved return fra controller (der returneres ikke JSON, jvf. 
+    // http://stackoverflow.com/questions/199099/how-to-manage-a-redirect-request-after-a-jquery-ajax-call)
+
+    //if (data.statusCode === 200) {
+    //    $('#FilesList tr:last').after(data.NewRow);
+    //    alert(data.status);
+    //}
+    //else {
+    //    alert(data.status);
+    //}
+    // $.unblockUI();
+    // nødvendigt med en redirect hvis success handleren skal kaldes da XHR requestet har naflet pipeline?
+    // window.location = 'http://localhost:30321/OrchardLocal/ProfileRedirect';
+
+    // der er åbenbart ikke en redirect? men en hardcoded url vil den tage
+    if (response.redirect) {
+        alert("ImageUpload.js - redirect to: " + response.redirect)
+        window.location.href = response.redirect;
+    }
+    else {
+        // Huu - ballade. Process the expected results...
+        window.location = 'http://localhost:30321/OrchardLocal/ProfileRedirect';
+    }
+}
+
+function errorHandler(xhr, ajaxOptions, thrownError) {
+    alert("There was an error attempting to upload the file. (" + thrownError + ")");
+}
+
+function OnDeleteAttachmentSuccess(data) {
+
+    if (data.ID && data.ID !== "") {
+        $('#Attachment_' + data.ID).fadeOut('slow');
+    }
+    else {
+        alter("Unable to Delete");
+        console.log(data.message);
+    }
+}
+
+function Cancel_btn_handler() {
+    $('#createView').empty();
+    $('.CreateLink').show();
+    // $.unblockUI();
+    window.location = 'http://localhost:30321/OrchardLocal';
+}
+
+
+/* Added functionality to enable JCrop */
+
+//************************************** JavaScript for cropping of image *****************************************
+
+// Q: scope correct with this?
+var imageCropWidth = 0;
+var imageCropHeight = 0;
+var cropPointX = 0;
+var cropPointY = 0;
+
+
+
+// TEST: Imagecontainer indsat istedet for eg $('#uploaded-image').Jcrop({
+function initCrop() {
+    $('#Imagecontainer').Jcrop({
+        onChange: setCoordsAndImgSize,
+        aspectRatio: 1
+    });
+
+    $("#hl-crop-image").on("click", function (e) {
+        e.preventDefault();
+        cropImage();
+    });
+}
+
+function destroyCrop() {
+    var jcropApi = $('#Imagecontainer').data('Jcrop');
+
+    if (jcropApi !== undefined) {
+        jcropApi.destroy();
+        $('#Imagecontainer').attr('style', "").attr("src", "");
+    }
+}
+
+function setCoordsAndImgSize(e) {
+
+    imageCropWidth = e.w;
+    imageCropHeight = e.h;
+
+    cropPointX = e.x;
+    cropPointY = e.y;
+}
+
+function cropImage() {
+
+    if (imageCropWidth === 0 && imageCropHeight === 0) {
+        alert("Please select crop area.");
+        return;
+    }
+
+    // when sending an AJAX request 'manually' in Orchard or MVC in general, the antiforgery token must be supplied too
+    var form = $('#FormUpload')[0];
+    var token = $('input[name="__RequestVerificationToken"]', form).val();
+    // alert("Value of AF token: " + token); // works as advertised!    
+
+    $.ajax({
+        url: 'http://localhost:30321/OrchardLocal/Image/CropImage',
+        type: 'POST',
+        data: {
+            imagePath: $("#hf-uploaded-image-path").val(), // denne sættes til url for posted pic
+            cropPointX: cropPointX,
+            cropPointY: cropPointY,
+            imageCropWidth: imageCropWidth,
+            imageCropHeight: imageCropHeight,
+            __RequestVerificationToken: token
+        },
+        success: function (data) {
+
+            alert("Success handler executing.");
+            $("#hf-cropped-image-path").val(data.photoPath);
+
+            $("#my-cropped-image")
+                .attr("src", data.photoPath + "?t=" + new Date().getTime())
+                .show();
+
+            $("#btn-my-submit").fadeIn("slow");
+        },
+        error: function (data) {
+            alert("Error handler executing.");
+        }
+    });
+}
+
+//************************************** JavaScript for cropping of image END **************************************
+
